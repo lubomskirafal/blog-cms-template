067acd4cd62a193c7735886dd8a5e793d1a38150
lubomskirafal <lubomski.rafal@gmail.com>
2020-10-16 10:09:02 +0200

    add articlesRouter

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const newsRouter = require('./routes/news');
+const articlesRouter = require('./routes/articles');
 const adminRouter = require('./routes/admin');
 
 const app = express();
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/news', newsRouter);
+app.use('/articles', articlesRouter);
 app.use('/admin', adminRouter);
 
 
