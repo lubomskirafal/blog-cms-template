a61b901a8b9d1870dfef32497f492ca4dc1f93d2
lubomskirafal <lubomski.rafal@gmail.com>
2020-10-17 12:03:57 +0200

    add nav locals.path & css

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next)=>{
+  res.locals.path = req.path;
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/articles', articlesRouter);
 app.use('/contact', contactRouter);
@@ -28,12 +33,12 @@ app.use('/admin', adminRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next)=> {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next)=> {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
