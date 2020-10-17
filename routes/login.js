731e03e3dc24eb90eef8bf0ce19745f29d5f05ba
lubomskirafal <lubomski.rafal@gmail.com>
2020-10-17 16:10:21 +0200

    fix breakpoints

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const email = 'luomskirafal@gmail.com';
+const password = '1234';
 
+const isValid = {
+    email: false,
+    password: false
+};
 
 router.get('/', (req, res)=> {
     res.render('login', { title: 'Admin login' });
+});
+
+router.post('/', (req, res)=> {
+    const body = req.body;
+
+    console.log(body)
+
+
+    res.redirect('/login');
 });
 
 module.exports = router;
