6597ff6151fc1b4537f7f307d0857d75cd6d7c42
lubomskirafal <lubomski.rafal@gmail.com>
2020-10-18 18:39:23 +0200

    add 'add article form'

--- models/article.js
+++ models/article.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 const articleSchema = new Schema({
     title: {type: String, required:true},
-    date: {type: Date, default: Date.now},
+    date: {type: String, required:true},
     tags:[],
     content: String,
     picUrl: String
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,25 @@ const express = require('express');
 const router = express.Router();
 const Article = require('../models/article');
 
+const getTimeStamp = ()=> {
+  const date = new Date();
+
+  const year = date.getFullYear();
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const i = date.getMonth()
+  const month = months[i];
+  const day = date.getDate();
+
+  const h = date.getHours();
+  const m = date.getMinutes();
+  const s = date.getSeconds();
+
+  const clock = `${h<10?'0'+h:h}:${m<10?'0'+m:m}:${s<10?'0'+s:s}`
+
+  const timeStamp = `${day} ${month} ${year} ${clock}`;
+
+  return timeStamp;
+};
 // router.all('*', (req, res, next)=> {
 //   //secure admin session
 //   if(!req.session.admin) {
@@ -16,7 +35,20 @@ const Article = require('../models/article');
 /* GET admin page. */
 router.get('/', (req, res)=> {
 
-  res.render('admin', { title: 'Admin panel' });
+  // const article = new Article({
+  //   title: 'Pierwszy wpis na blogu',
+  //   content: 'To jest pierwszy wpis. to jest zawartośc wpisu',
+  //   date: getTimeStamp()
+  // });
+  
+  // article.save(err => console.log(err));
+  
+  
+  res.render('admin/index', { title: 'Admin panel' });
+});
+
+router.get('/articles/add', (req, res) =>{
+  res.render('admin/articleForm', {title: "Create article"});
 });
 
 module.exports = router;
