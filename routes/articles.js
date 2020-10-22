4b8d48ae3e9e46a0710f6fae68149e568eb1726d
lubomskirafal <lubomski.rafal@gmail.com>
2020-10-22 15:09:36 +0200

    add subpage all articles layout

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Article = require('../models/article');
 
 /* GET home page. */
-router.get('/', (req, res, next)=> {
-  res.render('articles', { title: 'Articles' });
+router.get('/', (req, res)=> {
+  
+  Article.find({}, (err, articles) => {
+    
+    res.render('articles', { title: 'All articles', articles });
+  });
 });
 
 module.exports = router;
